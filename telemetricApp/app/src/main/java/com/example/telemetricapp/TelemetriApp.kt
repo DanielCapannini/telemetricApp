5dff17c8053ce42013463a00eb4d03e519c58067
DanielCapannini <daniel.capannini@studio.unibo.it>
2023-04-26 17:37:30 +0200

    ricostruzione app

--- telemetricApp/app/src/main/java/com/example/telemetricapp/TelemetriApp.kt
+++
@@ -1,8 +0,0 @@
-package com.example.telemetricapp
-
-sealed class AppScreen(val name: String) {
-    object Home : AppScreen("Home")
-    object Add : AppScreen("Add Screen")
-    object Details : AppScreen("Details Screen")
-    object Settings : AppScreen("Settings Screen")
-}
