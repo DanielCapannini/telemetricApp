e7f3f7d37d755585c63955da7a441821b7f2cd77
DanielCapannini <93661927+DanielCapannini@users.noreply.github.com>
2023-04-28 15:59:17 +0200

    ..

--- AppAndroid/app/src/main/java/com/example/appandroid/Navigation.kt
+++ AppAndroid/app/src/main/java/com/example/appandroid/Navigation.kt
@@ -1,9 +1,78 @@
 package com.example.appandroid
 
+import android.graphics.drawable.Icon
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.navigation.compose.rememberNavController
+
 sealed class AppScreen(val name: String) {
     object Home : AppScreen("Home")
     object Registration : AppScreen("Add Screen")
-    object Details : AppScreen("Details Screen")
+    object History : AppScreen("Details Screen")
     object Settings : AppScreen("Settings Screen")
+
+    object Session : AppScreen("Session Screen")
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopAppBar(
+    currentScreen: String,
+    canNavigateBack: Boolean,
+    navigateUp: () -> Unit,
+    modifier: Modifier = Modifier,
+    onSettingsButtonClicker: () -> Unit
+){
+    CenterAlignedTopAppBar(
+        title = {
+            Text(
+                text = currentScreen,
+                fontWeight = FontWeight.Medium,
+            )
+        },
+        modifier = modifier,
+        navigationIcon = {
+            if(canNavigateBack){
+                IconButton(onClick = navigateUp){
+                    Icon(
+                        imageVector = Icons.Filled.ArrowBack,
+                        contentDescription = "Back Button"
+                    )
+                }
+            }
+        },
+        actions = {
+            if(currentScreen != AppScreen.Settings.name){
+                IconButton(onClick = onSettingsButtonClicker){
+                    Icon(
+                        Icons.Filled.Settings,
+                        contentDescription = "Settings"
+                    )
+                }
+            }
+        },
+        colors = TopAppBarDefaults.smallTopAppBarColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer
+        )
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun NavigationApp(
+    navController: NavHostController = rememberNavController()
+){
+
 }
 
